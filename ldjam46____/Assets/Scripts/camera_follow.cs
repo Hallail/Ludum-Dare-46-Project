3e35b8bd10bcfbf2b9cebcb5c103bac81fcc7c87
Hallail <63903484+Hallail@users.noreply.github.com>
2020-04-18 15:32:14 +0300

    cam follow

---
+++ ldjam46____/Assets/Scripts/camera_follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour
+{
+    public float smoothness = 0.5f;
+    public Vector3 Offset = new Vector3(0, 5, 0);
+    public GameObject target;
+    private Transform transform;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.position =
+            Vector3.Lerp(target.transform.position, transform.position, smoothness)
+            + Offset;
+    }
+}
