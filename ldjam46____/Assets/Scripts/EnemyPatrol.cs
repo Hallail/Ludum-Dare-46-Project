94bdfe99c7e1c8f75f818c8a7cdb5cde35afa9fa
hhalsaif <53378715+hhalsaif@users.noreply.github.com>
2020-04-19 16:46:26 +0300

    Added Patrol Enemy

---
+++ ldjam46____/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform moveSpot;
+
+    public float minX;
+    public float maxX;
+    public float posY;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), posY);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, moveSpot.position) < 0.2f){
+            if(waitTime <= 0){
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), posY);
+                waitTime = startWaitTime;
+            }else{
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag=="Player")
+        SceneManager.LoadScene("Dead");
+    }
+}
